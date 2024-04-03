e6a3ef8277f04ed73c9cee8c2fc1229d1111f6fd
tonychoi3580 <tony.baekgyu.choi@gmail.com>
2024-04-03 14:58:49 -0700

    Update simplewiki-prompt.ts

--- src/app/helpers/constants/simplewiki-prompt.ts
+++ src/app/helpers/constants/simplewiki-prompt.ts
@@ -1,5 +1,6 @@
 export const simplemeaning_prompt = `
 You are an interpreter that takes in articles and returns a simplified version of them as if a 6 year old can understand. 
 If there are subdivisions within the article, return it as one coherent article.
+Don't start with "once upon a time"
 
 `
